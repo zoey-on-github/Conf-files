2faa292588c8c0b2002b7e3e2e03b4b51723118c
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2024-05-26 01:53:24 -0700

    more lua rewrites

--- init.lua
+++ init.lua
@@ -11,50 +11,45 @@ if (empty($TMUX))
     set termguicolors
   endif
 endif
-call plug#begin()
-	Plug 'vim-airline/vim-airline'
-	"Plug 'preservim/nerdtree' 
-	Plug 'rust-lang/rust.vim'
-	Plug 'andweeb/presence.nvim'
-	Plug 'junegunn/fzf'
-        Plug 'lukas-reineke/indent-blankline.nvim'
-	Plug 'voldikss/vim-floaterm'
-	Plug 'tpope/vim-fugitive'
-	Plug 'ms-jpq/coq_nvim', {'branch': 'coq'}
-	Plug 'ms-jpq/coq.artifacts', {'branch': 'artifacts'}
-	Plug 'ms-jpq/coq.thirdparty', {'branch': '3p'}
-	Plug 'easymotion/vim-easymotion'
-	Plug 'luochen1990/rainbow'
-	Plug 'nvim-lua/plenary.nvim'
-	Plug 'nvim-telescope/telescope.nvim', { 'tag': '0.1.3' }
-	Plug 'yuezk/vim-js'
-	Plug 'maxmellon/vim-jsx-pretty'	
-	Plug 'aymericbeaumet/vim-symlink'
-	Plug 'moll/vim-bbye'
-	Plug 'MunifTanjim/nui.nvim'
-	Plug 'stevearc/oil.nvim'
-	Plug 'nvim-neo-tree/neo-tree.nvim', {'branch':'v3.x'}
-	Plug 'llathasa-veleth/vim-brainfuck'
-        Plug 'mrcjkb/rustaceanvim'
-call plug#end()
 let g:airline#extensions#tabline#enabled = 1    
 ]])
-require("oil").setup()
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-require("coq_3p") {
-  { src = "builtin/ada"     },
-  { src = "builtin/c"       },
-  { src = "builtin/clojure" },
-  { src = "builtin/css"     },
-  { src = "builtin/haskell" },
-  { src = "builtin/html"    },
-  { src = "builtin/js"      },
-  { src = "builtin/php"     },
-  { src = "builtin/syntax"  },
-  { src = "builtin/xml"     },
-
-}
+local vim = vim
+local Plug = vim.fn['plug#']
+vim.call('plug#begin')
+	Plug('vim-airline/vim-airline')
+	Plug('rust-lang/rust.vim')
+	Plug('andweeb/presence.nvim')
+	Plug('junegunn/fzf')
+        Plug('lukas-reineke/indent-blankline.nvim')
+	Plug('voldikss/vim-floaterm')
+	Plug('tpope/vim-fugitive')
+	Plug('easymotion/vim-easymotion')
+	Plug('luochen1990/rainbow')
+	Plug('nvim-lua/plenary.nvim')
+	Plug('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.3' })
+	Plug('aymericbeaumet/vim-symlink')
+	Plug('moll/vim-bbye')
+	Plug('MunifTanjim/nui.nvim')
+	Plug('stevearc/oil.nvim')
+	Plug('nvim-neo-tree/neo-tree.nvim', {['branch'] ='v3.x'})
+	Plug('llathasa-veleth/vim-brainfuck')
+        Plug('neovim/nvim-lspconfig')
+        Plug('hrsh7th/cmp-nvim-lsp')
+        Plug('hrsh7th/cmp-buffer')
+        Plug('hrsh7th/cmp-path')
+        Plug('hrsh7th/cmp-cmdline')
+        Plug('hrsh7th/nvim-cmp')
+        Plug('mrcjkb/rustaceanvim')
+        Plug('hrsh7th/cmp-vsnip')
+        Plug('hrsh7th/vim-vsnip')
+        Plug('williamboman/mason.nvim')
+        Plug('williamboman/mason-lspconfig.nvim')
+vim.call ('plug#end')
+require("oil").setup()
 require("ibl").setup()
+local lsp = require "lspconfig"
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 vim.opt.number = true
 vim.opt.expandtab = true
 vim.opt.relativenumber = true
@@ -72,9 +67,114 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, {
 vim.cmd("autocmd vimenter * ++nested colorscheme gruvbox")
 vim.api.nvim_create_autocmd({ "VimEnter"}, {
         pattern = {"*"},
-        command = "Neotree" 
+        command = "Neotree"
 })
-vim.api.nvim_create_autocmd({ "VimEnter"}, {
-        pattern = {"*"},
-        command = "COQnow" 
-})
+require'lspconfig'.lua_ls.setup {
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      return
+    end
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT'
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME
+          -- Depending on the usage, you might want to add additional paths here.
+          -- "${3rd}/luv/library"
+          -- "${3rd}/busted/library",
+        }
+        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+        -- library = vim.api.nvim_get_runtime_file("", true)
+      }
+    })
+  end,
+  capabilities = capabilities,
+  settings = {
+    Lua = {}
+  }
+}
+require'lspconfig'.rust_analyzer.setup {
+  capabilities = capabilities,
+  settings = {
+    ['rust-analyzer'] = {
+      diagnostics = {
+        enable = false;
+      }
+    }
+  }
+}
+  local cmp = require'cmp'
+
+  cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+      end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
+  -- Set configuration for specific filetype.
+  --[[ cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'git' },
+    }, {
+      { name = 'buffer' },
+    })
+ })
+ require("cmp_git").setup() ]]-- 
+
+  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    }),
+    matching = { disallow_symbol_nonprefix_matching = false }
+  })
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+
